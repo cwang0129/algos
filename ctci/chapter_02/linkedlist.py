ba96a4e46e358d6b5c7cc4a1dc04ab0f11545dfe
Chen <chen@bluebottlecoffee.com>
2018-12-15 10:16:02 -0800

    linkedlist

---
+++ ctci/chapter_02/linkedlist.py
@@ -0,0 +1,49 @@
+class Node:
+	def __init__(self,data):
+		self.value = data
+		self.next = None
+
+class linkedlist:
+	def __init__(self):
+		self.head = None
+
+	def push(self, data):
+		newNode = Node(data)
+		newNode.next = self.head
+		self.head = newNode
+
+	def append(self, data):
+		newNode = Node(data)
+		cur = prev = self.head
+		while(cur):
+			prev = cur
+			cur = cur.next
+		prev.next = newNode
+
+	def insertAfter(self, prev, data):
+		cur = self.head
+		newNode = Node(data)
+		while(cur):
+			if cur.value == prev:
+				newNode.next = cur.next
+				cur.next = newNode
+			cur = cur.next
+
+	def printll(self):
+		while(self.head):
+			print(self.head.value)
+			self.head = self.head.next
+
+if __name__ == '__main__':
+	ll = linkedlist()
+	ll.push(1)
+	ll.push(2)
+	ll.push(9)
+	ll.insertAfter(2,5)
+	ll.printll()
+
+
+
+
+
+
